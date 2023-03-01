5157e5a82dc4363edd26b7482dacae181babe0b7
ThanTranNghiep <thantrannghiep@gmail.com>
2023-03-01 18:42:14 +0700

    source

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace KNT_Shop.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace KNT_Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
