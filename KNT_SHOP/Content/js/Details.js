7dfbf9c9eef891de7a31948ea541881e51c75b49
VeeNeil <80906756+VeeNeil@users.noreply.github.com>
2023-03-16 17:19:15 +0700

    hoan thanh regular express

--- KNT_SHOP/Content/js/Details.js
+++ KNT_SHOP/Content/js/Details.js
@@ -1,19 +1,8 @@
-﻿function add() {
-    const number = document.getElementById("inputQuantity")
-    var value = parseInt(number.value)
-    var newvalue = value+1
-    number.value = newvalue.toString()
-    
-}
-function subtract() {
-    const number = document.getElementById("inputQuantity")
-    var value = parseInt(number.value)
-    if (value > 1) {
-        var newvalue = value - 1
-        number.value = newvalue.toString()
-    }
-    else {
-        number.value = 1
-    }
-    
+﻿function constraint() {
+    const input = document.getElementById("inputQuantity");
+    const inputValue = input.value;
+    const regex = /^(?:100|[1-9]\d|\d)$/
+    if (!regex.test(inputValue)) {
+        input.value = "1"
+    } 
 }
